b3d4a5f69f2eca851c07d300efa179892fdf954f
Wesley Costa <wesley_costa@outlook.com>
2022-04-30 21:51:21 -0300

    tests: integration tests

--- src/ToDeFerias.Bookings.Core/Messages/CommandHandler.cs
+++ src/ToDeFerias.Bookings.Core/Messages/CommandHandler.cs
@@ -13,9 +13,7 @@ public abstract class CommandHandler
     public void NotifyError(string message) =>
         ValidationResult?.Errors.Add(new ValidationFailure(string.Empty, message));
 
-#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
     public async Task<CommandHandlerResult> SaveData(IUnitOfWork unitOfWork, object? responseCommand)
-#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
     {
         if (!await unitOfWork.Commit())
         {
